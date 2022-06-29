e25e030c6f10a39c20822e3d0db2fac115d7d25e
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-06-29 11:59:13 +0300

    Update CategoryTest.php

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -18,45 +18,104 @@
 
 		private Category $category;
 
-		public function testValidate()
+		public function __construct(?string $name = NULL, array $data = [], $dataName = '')
 		{
+			parent::__construct($name, $data, $dataName);
 			$this->category = new Category();
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate()
+		: void
+		{
 			try {
 				$this->category->validate("test", "test2");
 				$this->fail();
 			} catch (CategoryExtension $e) {
 				$this->assertTrue(TRUE);
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate1()
+		: void
+		{
 			try {
 				$this->category->validate("Люстры", "test2");
 				$this->fail();
 			} catch (CategoryExtension $e) {
 				$this->assertTrue(TRUE);
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate2()
+		: void
+		{
 			try {
 				$this->category->validate("Люстры", "Настенные светильники");
 				$this->fail();
 			} catch (CategoryExtension $e) {
 				$this->assertTrue(TRUE);
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate3()
+		: void
+		{
 			try {
 				$this->category->validate("Люстра", "Настенные светильники");
 				$this->fail();
 			} catch (CategoryExtension $e) {
 				$this->assertTrue(TRUE);
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate4()
+		: void
+		{
 			try {
 				$this->category->validate("Люстры", "Люстры");
 				$this->assertTrue(TRUE);
 			} catch (CategoryExtension $e) {
 				$this->fail();
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate5()
+		: void
+		{
 			try {
-				$this->category->validate("люстры", "Потолочные люстры");
+				[$category, $subCategory] = $this->category->validate("люстры", "ПоТоЛоЧнЫе лЮсТрЫ");
 				$this->assertTrue(TRUE);
+				$this->assertEquals('Люстры', $category);
+				$this->assertEquals("Потолочные люстры", $subCategory);
 			} catch (CategoryExtension $e) {
 				$this->fail();
 			}
+		}
+
+		/**
+		 * @return void
+		 */
+		public function testValidate6()
+		: void
+		{
 			try {
 				$this->category->validate("Люстры", "Люстры");
 				$this->assertTrue(TRUE);
